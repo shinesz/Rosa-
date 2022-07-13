1e094781637cf28ff2dbd4419dd01ac4ff7332e8
Bruna França Martinez <103595190+shinesz@users.noreply.github.com>
2022-07-13 14:57:55 -0300

    Add files via upload

---
+++ Rosa++/Rosa2.0/Rosa/Rosa/EventsForms.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.EntityFramework;
+using System.Globalization;
+
+namespace Rosa
+{
+    public partial class EventsForms : Form
+    {
+        public MySqlConnection conexao;
+
+        public EventsForms()
+        {
+            InitializeComponent();
+            
+            conexao = new MySqlConnection("server=localhost;DataBase=Rosa1;Uid=root;password=");
+            try
+            {
+                conexao.Open();//Tentando conectar ao BD
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Algo deu errado!\n\n" + erro);//Enviando a mesagem de erro aos usuários
+                conexao.Close();//fechando a conexão com o banco de dados
+            }
+        }//fim do DAOPessoa
+
+
+        private void EventsForms_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ Rosa++/Rosa2.0/Rosa/Rosa/Form4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rosa
+{
+    public partial class inicio : Form
+    {
+        Rosa entre;
+        Form6 cada;
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Entrar_Click(object sender, EventArgs e)
+        {
+            entre = new Rosa();
+            this.Visible = false;
+            entre.ShowDialog();
+            
+        }
+
+        private void cadastrar_Click(object sender, EventArgs e)
+        {
+            cada = new Form6();
+            cada.ShowDialog();
+        }
+    }
+}
